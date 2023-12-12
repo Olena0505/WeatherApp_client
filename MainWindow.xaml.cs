93d96af5f3b472ddceebf4f941e9f09da493f24c
Olena0505 <olenakit0505@gmail.com>
2023-12-12 07:33:34 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WeatherApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.ShowInTaskbar = true;
+
+            Start();
+        }
+
+        private void Start()
+        {
+
+        }
+
+        private MenuItem lastSelectedMenuItem;
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MenuItem clickedMenuItem = sender as MenuItem;
+
+            if (lastSelectedMenuItem != null && lastSelectedMenuItem != clickedMenuItem)
+            {
+                lastSelectedMenuItem.IsEnabled = true;
+            }
+
+            if (clickedMenuItem != null)
+            {
+                clickedMenuItem.IsEnabled = false;
+                lastSelectedMenuItem = clickedMenuItem;
+            }
+        }
+
+        private void Menu_Click(object sender, RoutedEventArgs e)
+        {
+            if (lastSelectedMenuItem != null)
+            {
+                lastSelectedMenuItem.IsEnabled = true;
+                lastSelectedMenuItem = null;
+            }
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        
+    }
+}
---
+++ UserControls/WeatherPerHourCard.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WeatherApp.UserControls
+{
+    /// <summary>
+    /// Interaction logic for WeatherPerHourCard.xaml
+    /// </summary>
+    public partial class WeatherPerHourCard : UserControl
+    {
+        public WeatherPerHourCard()
+        {
+            InitializeComponent();
+        }
+        public string Hour
+        {
+            get { return (string)GetValue(HourProperty); }
+            set { SetValue(HourProperty, value); }
+        }
+        public static readonly DependencyProperty HourProperty = DependencyProperty.Register("Hour", typeof(string), typeof(WeatherPerHourCard));
+
+        public BitmapImage Source
+        {
+            get { return (BitmapImage)GetValue(SourceProperty); }
+            set { SetValue(SourceProperty, value); }
+        }
+        public static readonly DependencyProperty SourceProperty = DependencyProperty.Register("Source", typeof(BitmapImage), typeof(WeatherPerHourCard));
+
+        public string Temperature
+        {
+            get { return (string)GetValue(TemperatureProperty); }
+            set { SetValue(TemperatureProperty, value); }
+        }
+        public static readonly DependencyProperty TemperatureProperty = DependencyProperty.Register("Temperature", typeof(string), typeof(WeatherPerHourCard));
+
+        public string Precipitation
+        {
+            get { return (string)GetValue(PrecipitationProperty); }
+            set { SetValue(PrecipitationProperty, value); }
+        }
+        public static readonly DependencyProperty PrecipitationProperty = DependencyProperty.Register("Precipitation", typeof(string), typeof(WeatherPerHourCard));
+
+        public string Wind
+        {
+            get { return (string)GetValue(WindProperty); }
+            set { SetValue(WindProperty, value); }
+        }
+        public static readonly DependencyProperty WindProperty = DependencyProperty.Register("Wind", typeof(string), typeof(WeatherPerHourCard));
+
+    }
+}
